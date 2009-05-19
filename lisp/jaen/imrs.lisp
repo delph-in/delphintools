4fe5c02bfec5d0b097a7cf75275b082c2fa69c2a
Eric Nichols <underspecified@gmail.com>
2009-05-19 17:40:35 +0900

    Made vmrs paraphrase Japanese and imrs paraphrase English in jaen.

--- lisp/jaen/imrs.lisp
+++ lisp/jaen/imrs.lisp
@@ -1,2 +1,2 @@
-(tsdb:tsdb :cpu :jacy :file t :count 1 :error :exit :wait 600)
-(lkb::read-script-file-aux (format nil "~a/dfki/jacy/lkb/script" root))
+(tsdb:tsdb :cpu :terg :file t :count 1 :error :exit :wait 600)
+(lkb::read-script-file-aux (format nil "~a/lingo/terg/lkb/script" root))
