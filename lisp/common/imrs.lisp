08af302817221ce00351acee2f1e47597771cc13
Eric Nichols <underspecified@gmail.com>
2009-05-21 08:24:07 +0900

    imrs.lisp: build profile with object instead of original.

--- lisp/common/imrs.lisp
+++ lisp/common/imrs.lisp
@@ -4,7 +4,7 @@
 (loop for n from 0 below limit do
   (let ((dir (format nil "imrs/~a" n)))
     (if (not (tsdb::verify-tsdb-directory dir :absolute nil))
-        (tsdb::do-import-items (format nil "~a/bitext/original" profile_top) 
+        (tsdb::do-import-items (format nil "~a/bitext/object" profile_top) 
           dir :format :bitext))
     (tsdb::tsdb-do-process dir :condition (format nil "result-id=~a" n)
-  	  :type :generate :overwrite t :gold "smrs") ))
+  	  :type :generate :overwrite t :gold "omrs") ))
