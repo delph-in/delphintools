ceca9a53819df7f43240d4ad5ae48c2505594f11
Eric Nichols <underspecified@gmail.com>
2009-05-21 07:47:20 +0900

    Added settings file for lisp.

---
+++ lisp/settings/generation.lisp
@@ -0,0 +1,8 @@
+(setf limit 1)
+(setf count 1)
+(setf tsdb::*tsdb-maximal-number-of-analyses* 100)
+(setf tsdb::*tsdb-maximal-number-of-results* 100)
+(set (intern "*MAXIMUM-NUMBER-OF-EDGES*" :lkb) 100000)
+(set (intern "*UNPACK-EDGE-ALLOWANCE*" :lkb) 100000)
+(set (intern "*TSDB-MAXIMAL-NUMBER-OF-EDGES*" :tsdb) 100000)
+
