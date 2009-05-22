342240978b224aa92931baac4d4a0392e1e8308e
Eric Nichols <underspecified@gmail.com>
2009-05-22 12:59:50 +0900

    lisp/jaen/pmrs.lisp: disabled TSDB loading CPU to allow partial transfer to work.

--- lisp/jaen/pmrs.lisp
+++ lisp/jaen/pmrs.lisp
@@ -1,2 +1,2 @@
-(tsdb:tsdb :cpu :jaen :file t :count count :error :exit :wait 600)
+;(tsdb:tsdb :cpu :jaen :file t :count count :error :exit :wait 600)
 (lkb::read-script-file-aux (format nil "~a/uio/jaen/lkb/script" root))
