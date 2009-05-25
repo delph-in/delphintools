0aae71ace5892864ac4f4fb6a59363330f18edee
Eric Nichols <underspecified@gmail.com>
2009-05-25 19:08:03 +0900

    Reverted jane/*.lisp to reading lkb/script.

--- lisp/jaen/imrs.lisp
+++ lisp/jaen/imrs.lisp
@@ -1,2 +1,2 @@
 (tsdb:tsdb :cpu :terg :file t :count count :error :exit :wait 600)
-;(lkb::read-script-file-aux (format nil "~a/lingo/terg/lkb/script" root))
+(lkb::read-script-file-aux (format nil "~a/lingo/terg/lkb/script" root))
