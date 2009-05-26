31fec9b8802f233eaa9cb9503d3554f526f047ca
Eric Nichols <underspecified@gmail.com>
2009-05-26 11:58:15 +0900

    Obsoleted imrs, omrs, and pmrs tasks in enen because they are ill-defined for English->English paraphrasing.

--- lisp/enen/omrs.lisp
+++
@@ -1,2 +0,0 @@
-(tsdb:tsdb :cpu :terg+tnt :file t :count count :error :exit :wait 600)
-(lkb::read-script-file-aux (format nil "~a/lingo/terg/lkb/script" root))
--- lisp/enen/pmrs.lisp
+++
@@ -1,2 +0,0 @@
-(tsdb:tsdb :cpu :enen :file t :count count :error :exit :wait 600)
-(lkb::read-script-file-aux (format nil "~a/uio/enen/lkb/script" root))
