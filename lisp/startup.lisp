78db2ac416da14bdcfb8c26e81ea5ab30d26787d
Eric Nichols <underspecified@gmail.com>
2009-06-02 18:00:43 +0900

    Moved unsetting of $LUI from bin/logon_do to lisp/startup.lisp.

--- lisp/startup.lisp
+++ lisp/startup.lisp
@@ -1,5 +1,6 @@
 (setf root (namestring (parse-namestring (getenv "LOGONROOT"))))
 (setf (system:getenv "DISPLAY") nil)
+(setf (system:getenv "LUI") nil)
 (pushnew :lkb *features*)
 (pushnew :mrs *features*)
 (pushnew :tsdb *features*)
