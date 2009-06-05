1231a3a485a5bc0f02a9251325b6c04b1099ac1c
darren scott appling <darren.scott.appling@gmail.com>
2009-06-05 12:27:59 +0900

    GPMRS uses PMRS results to do generation. A PMRS file was created for the EnEn system.

---
+++ lisp/common/gpmrs.lisp
@@ -0,0 +1,14 @@
+(lkb::index-for-generator)
+(tsdb::tsdb :home profile_top)
+(tsdb::tsdb :list)
+(loop for n from 0 below parse-limit do
+  (loop for m from 0 below transfer-limit do
+    (let ((dir (format nil "gpmrs/~a/~a" n m)))
+      (if (not (tsdb::verify-tsdb-directory dir :absolute nil))
+          (if (equal fmt "ascii")
+              (tsdb::do-import-items (format nil "~a/bitext/ascii" profile_top) 
+                dir :format :ascii)
+              (tsdb::do-import-items (format nil "~a/bitext/original" profile_top) 
+                dir :format :bitext)) )
+      (tsdb::tsdb-do-process dir :condition (format nil "result-id=~a" m)
+  	    :type :generate :overwrite t :gold (format nil "pmrs/~a" n)) )))
---
+++ lisp/settings/enen_gpmrs.lisp
@@ -0,0 +1,10 @@
+(setf fmt "ascii")
+(setf limit 1) ;; doesn't exist now..
+(setf parse-limit 1)
+(setf transfer-limit 10)
+(setf count 1)
+(setf tsdb::*tsdb-maximal-number-of-analyses* 100)
+(setf tsdb::*tsdb-maximal-number-of-results* 100)
+(set (intern "*MAXIMUM-NUMBER-OF-EDGES*" :lkb) 100000)
+(set (intern "*UNPACK-EDGE-ALLOWANCE*" :lkb) 100000)
+(set (intern "*TSDB-MAXIMAL-NUMBER-OF-EDGES*" :tsdb) 100000)
