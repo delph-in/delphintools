0b6d6c66435a7d192f99ebaef6ce7b7c65b8c349
Eric Nichols <underspecified@gmail.com>
2009-06-19 17:40:42 +0900

    Reverted from ERG trunk (terg) to main branch.

--- lisp/jaen/imrs.lisp
+++ lisp/jaen/imrs.lisp
@@ -1,2 +1,2 @@
-(tsdb:tsdb :cpu :terg :file t :count count :error :exit :wait 600)
-(lkb::read-script-file-aux (format nil "~a/lingo/terg/lkb/script" root))
+(tsdb:tsdb :cpu :erg :file t :count count :error :exit :wait 600)
+(lkb::read-script-file-aux (format nil "~a/lingo/erg/lkb/script" root))
