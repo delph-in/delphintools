4749dc5124d2e636670f57e289dcb96c5a2c3771
darren scott appling <darren.scott.appling@gmail.com>
2009-07-17 23:58:47 -0400

    Updated Edges for Generation

--- lisp/settings/paraphrasing.lisp
+++ lisp/settings/paraphrasing.lisp
@@ -3,6 +3,6 @@
 (setf count 1)
 (setf tsdb::*tsdb-maximal-number-of-analyses* 100)
 (setf tsdb::*tsdb-maximal-number-of-results* 100)
-(set (intern "*MAXIMUM-NUMBER-OF-EDGES*" :lkb) 100000)
+(set (intern "*MAXIMUM-NUMBER-OF-EDGES*" :lkb) 10000)
 (set (intern "*UNPACK-EDGE-ALLOWANCE*" :lkb) 100000)
-(set (intern "*TSDB-MAXIMAL-NUMBER-OF-EDGES*" :tsdb) 100000)
+(set (intern "*TSDB-MAXIMAL-NUMBER-OF-EDGES*" :tsdb) 10000)
