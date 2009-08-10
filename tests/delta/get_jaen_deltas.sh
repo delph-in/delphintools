27684c3f910051e800013ab208bd4e5184a09ea6
Eric Nichols <underspecified@gmail.com>
2009-08-10 14:38:02 +0900

    Added delta test set.

---
+++ tests/delta/get_jaen_deltas.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+profile2deltas `for n in 000 001 002 $(seq -w 006 100); do echo ~/tbs/ja2en/tanaka-$n.jaen; done` | tee sd.txt
+sd2deltas.py < sd.txt | tee tc-deltas.txt
+sort -k3n -k2n tc-deltas.txt | sed 's/ /\t/g' > tc-deltas.sorted.txt
+grep -iv '_EF'  tc-deltas.sorted.txt > $HOME/www/data/tc-deltas.sorted.txt
