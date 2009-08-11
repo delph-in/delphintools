a7342a8b964c4f2f468170154b3eded0b2c05c6c
Eric Nichols <underspecified@gmail.com>
2009-08-11 17:57:09 +0900

    Updated delta test.

--- tests/delta/get_jaen_deltas.sh
+++ tests/delta/get_jaen_deltas.sh
@@ -3,4 +3,4 @@
 profile2deltas `for n in 000 001 002 $(seq -w 006 100); do echo ~/tbs/ja2en/tanaka-$n.jaen; done` | tee sd.txt
 sd2deltas.py < sd.txt | tee tc-deltas.txt
 sort -k3n -k2n tc-deltas.txt | sed 's/ /\t/g' > tc-deltas.sorted.txt
-grep -iv '_EF'  tc-deltas.sorted.txt > $HOME/www/data/tc-deltas.sorted.txt
+grep -Ei -e '-O?MTR' tc-deltas.sorted.txt > $HOME/www/data/tc-deltas.sorted.txt
